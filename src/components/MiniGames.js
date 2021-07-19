199aafb217b4478ca091bf90be1c3e460a1f596b
tam <cherykim123@gmail.com>
2021-07-19 02:54:46 -0700

    moving style backgroundImage

--- src/components/MiniGames.js
+++ src/components/MiniGames.js
@@ -1,15 +1,10 @@
 import React from "react";
 import GameCard from "./GameCard";
-import HomeGameBackground from "../bgr.png";
 import data from "./../data";
-import "../styles/games.css";
 
 function MiniGames(props) {
   return (
-    <div
-      className="HomeBackground"
-      style={{ backgroundImage: `url(${HomeGameBackground})` }}
-    >
+    <div className="HomeBackground">
       <div className="container">
         <div className="row">
           {data.games.map((game) => (
