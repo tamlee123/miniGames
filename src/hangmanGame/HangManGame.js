c97f3120449ece615239e0460cb2e8c16df88b7a
tam <cherykim123@gmail.com>
2021-07-21 17:45:36 -0700

    represent array for guessed word-keyboard of 26 characters - handleGuess event

--- src/hangmanGame/HangManGame.js
+++ src/hangmanGame/HangManGame.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./HangManGame.css";
 class HangManGame extends Component {
   static defaultProps = {
     maxWrong: 6,
@@ -13,12 +13,46 @@ class HangManGame extends Component {
       "/images/6.jpg",
     ],
   };
-  state = { nWrong: 0, answer: "apple" };
-  render() {
+  state = { nWrong: 0, guessed: new Set(), answer: "apple" };
+
+  guessedWord() {
     return (
-      <div>
+      this.state.answer
+        .split("") //split word into an array
+
+        //map each character into a new array
+        //the array is either going to split out the character & _ or the actual letter
+        .map((ltr) => (this.state.guessed.has(ltr) ? ltr : "_"))
+    );
+  }
+  handleGuess = (e) => {
+    let ltr = e.target.value;
+    this.setState((st) => ({
+      guessed: st.guessed.add(ltr),
+      nWrong: st.nWrong + (st.answer.includes(ltr) ? 0 : 1),
+    }));
+  };
+  generateButtons() {
+    //split 26 character into an array, and map each character into a new array
+    return "abcdefghijklmnopqrstuvwxyz".split("").map((ltr) => (
+      //each character has a button
+      <button
+        value={ltr}
+        onClick={this.handleGuess}
+        disabled={this.state.guessed.has(ltr)}
+      >
+        {ltr}
+      </button>
+    ));
+  }
+  render() {
+    let gameState = this.generateButtons();
+    return (
+      <div className="HangMan">
         <h1>Hang Man</h1>
         <img src={this.props.images[this.state.nWrong]} alt="hangman"></img>
+        <p className="hangman-word">{this.guessedWord}</p>
+        <p>{gameState}</p>
       </div>
     );
   }
